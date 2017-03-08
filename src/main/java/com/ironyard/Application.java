606ffbb2ef454dbee159e159284d1444798206d6
Wail M. Yousif <wailm.yousif@Wails-MacBook-Pro.local>
2017-03-08 12:23:07 -0500

    Wail's first commit

--- src/main/java/com/ironyard/Application.java
+++ src/main/java/com/ironyard/Application.java
@@ -1,16 +1,159 @@
 package com.ironyard;
 
+import com.ironyard.data.Game;
+import com.ironyard.data.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.ironyard")
-public class Application {
+public class Application
+{
+    private static final int NUMBER_OF_GAMES = 25;
+    private int gameNumber;
+    private int gamesEnded;
 
-    public static void main(String[] args) {
+
+    private String doGame(Game aGame, int numOfPlayers)
+    {
+        int localGameNumber;
+        synchronized (this) {
+            gameNumber++;
+            localGameNumber = gameNumber;
+        }
+
+        System.out.println("Starting game #" + localGameNumber);
+
+        aGame.initGame(numOfPlayers);
+        aGame.dealOutAllCards();
+
+        Player winner = null;
+        while (winner == null)
+        {
+            aGame.playRound();
+            winner = aGame.getWinner();
+        }
+
+        synchronized (this) {
+            gamesEnded++;
+        }
+        String declareWinner = "Winner of game#" + String.valueOf(localGameNumber) + " is: " + winner.getName() +
+                "  (Games started = " + gameNumber + ", Games ended = " + gamesEnded + ").";
+        System.out.println(declareWinner);
+        return declareWinner;
+    }
+
+
+    private void doGameThreads_Method1() throws ExecutionException, InterruptedException
+    {
+        System.out.println("STARTING ALL GAMES\n-----------------------");
+
+        ExecutorService executor = Executors.newWorkStealingPool();
+        gameNumber = 0;
+        gamesEnded = 0;
+
+        List<Callable<String>> callables = new ArrayList<>();
+        for (int i=0; i < NUMBER_OF_GAMES; i++)
+        {
+            callables.add(() -> doGame(new Game(), 5));
+        }
+
+        executor.invokeAll(callables)
+                .stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    }
+                    catch (Exception e) {
+                        throw new IllegalStateException(e);
+                    }
+                });
+                //.forEach(System.out::println);
+
+        try
+        {
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.MILLISECONDS);
+        }
+        catch (InterruptedException e)
+        {
+            System.err.println("doGameThreads_Method1: tasks interrupted " + e.getMessage());
+        }
+        finally
+        {
+            if (!executor.isTerminated()) {
+                System.err.println("doGameThreads_Method1: cancel non-finished tasks to shutdown");
+            }
+            executor.shutdownNow();
+
+            System.out.println("ALL GAMES ENDED!\n-----------------------");
+        }
+    }
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         SpringApplication.run(Application.class, args);
+
+        (new Application()).doGameThreads_Method1();
+
+        //doGameThreads_Method2(5);
     }
+
+
+    /*
+    private void doGameThreads_Method2(Integer numberOfGames) throws ExecutionException, InterruptedException
+    {
+        Callable<Integer> task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                return 123;
+            }
+            catch (InterruptedException e) {
+                throw new IllegalStateException("task interrupted", e);
+            }
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfGames);
+
+        Future<Integer>[] futures = new Future[numberOfGames];
+
+        List<Future> activeGames = new ArrayList<>();
+
+        for (int i=0; i < numberOfGames; i++)
+        {
+            futures[i] = executor.submit(task);
+            activeGames.add(futures[i]);
+        }
+
+
+        while (activeGames.size() > 0)
+        {
+            boolean gameDone = false;
+            int i=0;
+
+            for (i=0; i < activeGames.size(); i++)
+            {
+                if (futures[i].isDone())
+                {
+                    gameDone = true;
+                    break;
+                }
+            }
+
+            if (gameDone)
+            {
+                System.out.println(futures[i].get());
+                activeGames.remove(i);
+            }
+        }
+
+    }
+     */
 
 }
--- src/main/java/com/ironyard/data/Game.java
+++ src/main/java/com/ironyard/data/Game.java
@@ -3,6 +3,8 @@ package com.ironyard.data;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by jasonskipper on 10/20/16.
@@ -45,6 +47,9 @@ public class Game {
         this.turns = turns;
     }
 
+    ExecutorService executor;
+
+    /*
     public static void main(String[] args){
         Game g = new Game();
         g.initGame(4);
@@ -53,8 +58,14 @@ public class Game {
 
         System.out.printf("\n\n%s is the Winner! In %s turns\n", winner.getName(),g.turns);
     }
+    */
 
     public void initGame(int numberOfPlayers){
+
+        //executor = Executors.newFixedThreadPool(numberOfPlayers);
+        //executor = Executors.newWorkStealingPool(numberOfPlayers);
+        executor = Executors.newWorkStealingPool();
+
         // set all lists to new empty lists
         turns = 0;
         players = new ArrayList<>();
@@ -85,14 +96,211 @@ public class Game {
         }
 
     }
+
+    ReentrantLock lock = new ReentrantLock();
+
+    private void playCardTest(Player p)
+    {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Void> future = executor.submit(new Callable<Void>() {
+            public Void call() {
+                Card t = p.playCard();
+                if(t != null) {
+                    lock.lock();
+                    cardsInPlay.add(t);
+                    lock.unlock();
+                }
+                return null;
+            }
+        });
+
+        try
+        {
+            future.get();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Exception=" + ex.getMessage());
+        }
+
+
+        /*
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            Card t = p.playCard();
+            if(t != null) {
+                lock.lock();
+                cardsInPlay.add(t);
+                lock.unlock();
+            }
+        });
+        */
+
+        /*
+        Runnable runnable = () -> {
+            Card t = p.playCard();
+            if(t != null) {
+                lock.lock();
+                cardsInPlay.add(t);
+                lock.unlock();
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+        */
+    }
+
+
+    private Integer onePlayerPlayCard(Player p)
+    {
+        Card t = p.playCard();
+        if(t !=null) {
+            synchronized (this) {
+                cardsInPlay.add(t);
+            }
+
+        }
+        return 0;
+    }
+
+
+    private void playCardsOfOneTurn()
+    {
+        ExecutorService localExecutor = null;
+
+        try
+        {
+            //localExecutor = Executors.newFixedThreadPool(players.size());
+            localExecutor = Executors.newWorkStealingPool();
+
+            List<Callable<Integer>> callables = new ArrayList<>();
+            for(Player p:players)
+            {
+                callables.add(() -> onePlayerPlayCard(p));
+            }
+
+            /*
+            List<Future<Integer>> futures = localExecutor.invokeAll(callables);
+            for (Future f: futures)
+            {
+                f.get();
+            }
+            */
+
+            executor.invokeAll(callables)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        }
+                        catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    });
+                    //.forEach(System.out::println);
+
+
+            try
+            {
+                localExecutor.shutdown();
+                localExecutor.awaitTermination(5, TimeUnit.MILLISECONDS);
+            }
+            catch (InterruptedException e)
+            {
+                System.err.println("playCardsOfOneTurn: tasks interrupted: " + e.getMessage());
+            }
+            finally
+            {
+                if (!localExecutor.isTerminated()) {
+                    System.err.println("playCardsOfOneTurn: cancel non-finished tasks to shutdown");
+                }
+                localExecutor.shutdownNow();
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("playCardsOfOneTurn: exception " + ex.getMessage());
+        }
+        finally {
+            if (localExecutor != null)
+            {
+                //((ThreadPoolExecutor)localExecutor).purge();
+            }
+        }
+    }
+
+
+
+
+    private void Test2()
+    {
+        CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executor);
+        for(Player p:players)
+        {
+            completionService.submit(new Callable<Integer>() {
+                public Integer call() {
+                    Card t = p.playCard();
+                    if(t !=null) {
+                        //synchronized (this) {
+                            cardsInPlay.add(t);
+                        //}
+                    }
+                    return 0;
+                }
+            });
+        }
+
+        int done = 0;
+        boolean exceptionOccurred = false;
+        while (done < players.size() && !exceptionOccurred)
+        {
+            try
+            {
+                Future<Integer> future = completionService.take();
+                Integer returnedValue = future.get();
+                done++;
+            }
+            catch (Exception ex)
+            {
+                exceptionOccurred = true;
+                System.out.println("playCardsOfOneTurn2: exception " + ex.getMessage());
+            }
+        }
+
+        try
+        {
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.MILLISECONDS);
+        }
+        catch (InterruptedException e)
+        {
+            System.err.println("playCardsOfOneTurn: tasks interrupted");
+        }
+        finally
+        {
+            if (!executor.isTerminated()) {
+                System.err.println("playCardsOfOneTurn: cancel non-finished tasks to shutdown");
+            }
+            executor.shutdownNow();
+        }
+    }
+
+
+
+
     public void playRound(){
             // each player puts down top card
+
+        playCardsOfOneTurn();
+/*
             for(Player p:players){
                 Card t = p.playCard();
                 if(t !=null) {
                     cardsInPlay.add(t);
                 }
+
             }
+*/
             turns++;
 
             // see who won
@@ -110,6 +318,7 @@ public class Game {
             cardsInPlay.clear();
 
     }
+
     public Player getWinner(){
         Player winner = null;
         for(Player p:players){
